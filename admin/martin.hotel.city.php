b08dcc7552d844fe4f4cc6c7491a28073aa57d44
china.codehome <china.codehome@gmail.com>
2010-07-03 16:15:41 +0000

    a

--- admin/martin.hotel.city.php
+++ admin/martin.hotel.city.php
@@ -45,7 +45,7 @@ switch($action)
 		break;
 	
 	case "save":
-		$city_alias = isset($_POST['city_name']) ? addslashes($_POST['city_name']) : '';
+		$city_alias = isset($_POST['city_alias']) ? addslashes($_POST['city_alias']) : '';
 		$city_parentid = isset($_POST['city_parentid']) ? intval($_POST['city_parentid']) : 0;
 		$city_alias = $city_parentid ? '' : $city_alias;
 		$HotelCityObj->setVar('city_id',$id);
