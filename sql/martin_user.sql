345d57ab1624964558b69f95297eb150f5e0053b
china.codehome <china.codehome@gmail.com>
2010-07-03 08:48:17 +0000

    a

--- sql/martin_user.sql
+++ sql/martin_user.sql
@@ -1,6 +1,6 @@
-ALTER TABLE  `7mogjl_users` ADD  `phone` VARCHAR( 25 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER  `user_mailok` ,
-ADD  `telephone` VARCHAR( 30 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER  `phone` ,
-ADD  `company` VARCHAR( 255 ) CHARACTER SET ucs2 COLLATE ucs2_general_ci NOT NULL AFTER  `telephone` ,
-ADD  `nationality` INT( 11 ) NOT NULL AFTER  `company` ,
-ADD  `document` INT( 11 ) NOT NULL AFTER  `nationality` ,
-ADD  `document_value` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER  `document`
+ALTER TABLE  `7mogjl_users` ADD  `phone` VARCHAR( 25 ) CHARACTER SET utf8 COLLATE utf8_general_ci AFTER  `user_mailok` DEFAULT '' ,
+ADD  `telephone` VARCHAR( 30 ) CHARACTER SET utf8 COLLATE utf8_general_ci  AFTER  `phone` DEFAULT '',
+ADD  `company` VARCHAR( 255 ) CHARACTER SET ucs2 COLLATE ucs2_general_ci  AFTER  `telephone` DEFAULT '',
+ADD  `nationality` INT( 11 )  AFTER  `company` DEFAULT 0,
+ADD  `document` INT( 11 )  AFTER  `nationality` DEFAULT 0,
+ADD  `document_value` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci AFTER  `document` DEFAULT '';
