9fd7809d1677861543ea96ab7b65713b24764358
china.codehome@gmail.com <china.codehome@gmail.com@a906bf62-ac41-9fef-b11a-38dddff18d0d>
2010-07-03 16:11:33 +0000

    a

--- trunk/admin/martin.hotel.city.php
+++ trunk/admin/martin.hotel.city.php
@@ -45,9 +45,13 @@ switch($action)
 		break;
 	
 	case "save":
+		$city_alias = isset($_POST['city_name']) ? addslashes($_POST['city_name']) : '';
+		$city_parentid = (isset($_POST['city_parentid']) ? intval($_POST['city_parentid']) : 0;
+		$city_alias = $city_parentid ? '' : $city_alias;
 		$HotelCityObj->setVar('city_id',$id);
-		$HotelCityObj->setVar('city_parentid', (isset($_POST['city_parentid'])) ? intval($_POST['city_parentid']) : 0);
-		$HotelCityObj->setVar('city_name', (isset($_POST['city_name'])) ? addslashes($_POST['city_name']) : 0);
+		$HotelCityObj->setVar('city_parentid', $city_parentid);
+		$HotelCityObj->setVar('city_name', (isset($_POST['city_name'])) ? addslashes($_POST['city_name']) : '');
+		$HotelCityObj->setVar('city_alias', $city_alias);
 		$HotelCityObj->setVar('city_level',  0);
 
 		if(!$id) $HotelCityObj->setNew();
